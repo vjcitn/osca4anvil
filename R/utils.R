13e7e1ef1dbe00f1a7fd2a15ca96a2dbc02f9d69
vjcitn <stvjc@channing.harvard.edu>
2021-11-07 07:15:15 -0500

    improve show_packs

--- R/utils.R
+++ R/utils.R
@@ -7,5 +7,10 @@ wr_cran = function(x) sprintf("<a href='https://CRAN.R-project.org/package=%s'>%
 #' @param x character(1) package name
 #' @param vers character(1) tag for a version, defaults to '3.13'
 #' @export
-wr_bioc = function(x, vers="3.13")
-function(x) sprintf("<a href='https://bioconductor.org/packages/%s/bioc/html/%s.html'>%s</a>", vers, x, x)
+wr_bioc = function(x, vers="3.13") sprintf("<a href='https://bioconductor.org/packages/%s/bioc/html/%s.html'>%s</a>", vers, x, x)
+
+#' build HTML anchor for Bioconductor experiment package pages
+#' @param x character(1) package name
+#' @param vers character(1) tag for a version, defaults to '3.13'
+#' @export
+wr_bioc_expt = function(x, vers="3.13") sprintf("<a href='https://bioconductor.org/packages/%s/data/experiment/html/%s.html'>%s</a>", vers, x, x)
