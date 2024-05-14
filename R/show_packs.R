c44848c1282fd8f310cc4aacafb9ca3c0d5eb674
vjcitn <stvjc@channing.harvard.edu>
2024-05-14 16:17:49 -0400

    update to remove obsolete material

--- R/show_packs.R
+++
@@ -1,8 +0,0 @@
-#' view a datatable of packages involved in book
-#' @importFrom DT datatable
-#' @param vers character(1) bioconductor version assumed in use
-#' @export
-show_packs = function(vers="3.13") {
- if (vers=="3.13") DT::datatable(osca4anvil::scpkginfo_3.13[,c("pkg_link", "Description")], escape=FALSE, options=list(pageLength=7))
- else stop("only supporting vers == 3.13 at this time")
-}
